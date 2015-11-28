ce859b669212668d88fec3be3fff0af32bb23a64
matsu490 <imototoriwatomodatijanai@gmail.com>
2015-11-28 17:49:35 +0900

    modified template.py

--- .vim/template/template.py
+++ .vim/template/template.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-#
 # vim: set fileencoding=utf-8 :
+# vim: set foldmethod=marker commentstring=\ \ #\ %s :
 #
-# Author:   Taishi Matsumura
+# Author:    Taishi Matsumura
 # Created:   <+DATE+>
+#
+# Copyright (C) 2015 Taishi Matsumura
 #
 <+CURSOR+>
